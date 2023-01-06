99d1d52a0174c4b6a8b1f234d64b07a39e5ec503
accademiafacile <info@vitoesposito.it>
2023-01-06 15:14:11 +0100

    a

---
+++ scripts/slider.js
@@ -0,0 +1,15 @@
+(document).ready(function() {
+    ('.testimonial-slider-container').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      speed: 500,
+      arrows: false,
+      accessibility: true,
+      dots: true,
+      fade: false,
+      infinite: true,
+      pauseOnHover: true,
+      pauseOnDotsHover: true
+    });
+  });
+  
